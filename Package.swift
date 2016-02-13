9108e3f4b4d0ebfa5ea347f37efb2698c1256b9d
Daniel Haight <Daniel1of1@users.noreply.github.com>
2016-02-13 21:25:28 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,26 +11,6 @@ let package = Package(
     targets: [
         Target(
             name: "RxSwift"
-        ),
-        Target(
-            name: "RxTests",
-            dependencies: [
-                .Target(name: "RxSwift")
-            ]
-        ),
-        Target(
-            name: "RxBlocking",
-            dependencies: [
-                .Target(name: "RxSwift")
-            ]
-        ),
-        Target(
-            name: "AllTests",
-            dependencies: [
-                .Target(name: "RxSwift"),
-                .Target(name: "RxBlocking"),
-                .Target(name: "RxTests")
-            ]
         )
     ]
 )
